83dfd7934f44fc0648cd498033a42a06ffd42af6
orlovdumitru <orlov_dumitru@yahoo.com>
2020-05-26 19:46:33 -0500

    first commit

---
+++ algorithms.py
@@ -0,0 +1,48 @@
+
+data = [2, 4, 5, 7, 8, 9, 12, 14, 17, 19, 22, 25, 27, 28, 33, 37]
+
+target = 27
+# linear search
+def linear_search(data, target):
+    for i in range(len(data)):
+        if data[i] == target:
+            return True
+    return False
+
+# print(linear_search(data, target))
+
+# ======================================================================
+# Iterative Binary search
+def binary_search_iterative(data, target):
+    low = 0
+    high = len(data)
+
+    while low <= high:
+        mid = (low + high) // 2
+        if target == data[mid]:
+            return True
+        elif target < data[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    
+    return False
+
+# print(binary_search_iterative(data, target))
+
+# ==================================================================
+# Recursive Binary Search
+def binary_search_recursive(data, target, low, high):
+    if low > high:
+        return False
+    else:
+        mid = (low + high) // 2
+        if target == data[mid]:
+            return True
+        elif target < data[mid]:
+            return binary_search_recursive(data, target, low, mid-1)
+        else:
+            return binary_search_recursive(data, target, mid+1, high)
+# print(binary_search_recursive(data, target, 0, len(data)-1))
+
+# ================================================================
